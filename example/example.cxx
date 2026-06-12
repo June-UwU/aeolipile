5c9819622d15a311cd370e7fd84b7141d1b76ba0
June-UwU <williamreji66@gmail.com>
2026-06-13 01:55:24 +0530

    [CORE] initialize build and logging infra 	1. added logging support with medusa. 	2. cmake build system integration. 	3. core lib compilation. 	4. error type inclusion. 	5. example application.
    Change-Id: I9241b89588545e4e28db7bf058d3265256f85ddd
    Signed-off-by: June-UwU <williamreji66@gmail.com>

---
+++ example/example.cxx
@@ -0,0 +1,7 @@
+#include "aeoilpile.hpp"
+#include "types.hpp"
+
+int main() {
+  aeo::initialize_early_infrastucture();
+  return 0;
+}
---
+++ include/types.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include <cstdint>
+
+using result_t = int64_t;
+using s8 = int8_t;
+using s16 = int16_t;
+using s32 = int32_t;
+using s64 = int64_t;
+
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+using f32 = float;
+using f64 = double;
+
+constexpr const result_t SUCCESS = 0x0;
+constexpr const result_t E_INVAL = 0x1;
