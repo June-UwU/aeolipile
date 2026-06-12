a0f19343f891990692e9f6fe91e10cdc6ea502bd
June-UwU <williamreji66@gmail.com>
2026-06-13 01:55:24 +0530

    [CORE] initialize build and logging infra
    	1. added logging support with medusa.
    	2. cmake build system integration.
    	3. core lib compilation.
    	4. error type inclusion.
    	5. example application.
    
    Change-Id: I9241b89588545e4e28db7bf058d3265256f85ddd
    Signed-off-by: June-UwU <williamreji66@gmail.com>

---
+++ utils/log.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "types.hpp"
+#include <medusa.hpp>
+
+namespace aeo {
+namespace logging {
+
+result_t initialize();
+result_t deinitialize();
+
+}; // namespace logging
+}; // namespace aeo
+
+#define LOGL(format, ...)                                                      \
+  medusa::write_log(medusa::log_level::log, format __VA_OPT__(, ) __VA_ARGS__)
+#define LOGP(format, ...)                                                      \
+  medusa::write_log(medusa::log_level::paranoid,                               \
+                    format __VA_OPT__(, ) __VA_ARGS__)
+#define LOGW(format, ...)                                                      \
+  medusa::write_log(medusa::log_level::warning,                                \
+                    format __VA_OPT__(, ) __VA_ARGS__)
+#define LOGF(format, ...)                                                      \
+  medusa::write_log(medusa::log_level::fatal, format __VA_OPT__(, ) __VA_ARGS__)
+
+#define TLOGL(format, ...)                                                     \
+  medusa::write_timed_log(medusa::log_level::log,                              \
+                          format __VA_OPT__(, ) __VA_ARGS__)
+#define TLOGP(format, ...)                                                     \
+  medusa::write_timed_log(medusa::log_level::paranoid,                         \
+                          format __VA_OPT__(, ) __VA_ARGS__)
+#define TLOGW(format, ...)                                                     \
+  medusa::write_timed_log(medusa::log_level::warning,                          \
+                          format __VA_OPT__(, ) __VA_ARGS__)
+#define TLOGF(format, ...)                                                     \
+  medusa::write_timed_log(medusa::log_level::fatal,                            \
+                          format __VA_OPT__(, ) __VA_ARGS__)
