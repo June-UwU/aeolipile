1951cd7f495770572d44c7b34c39596c2ecc54e0
June-UwU <williamreji66@gmail.com>
2026-06-13 03:15:55 +0530

    [CORE] added basic window support 	1. added one error code. 	2. integrated glfw. 	3. added error string support. 	4. added example.
    Change-Id: I32d90cd2001af5441219148891226c3a0398cc55
    Signed-off-by: June-UwU <williamreji66@gmail.com>

--- core/aeolipile.cxx
+++ core/aeolipile.cxx
@@ -2,9 +2,43 @@
 #include "log.hpp"
 
 namespace aeo {
+constexpr u32 version = medusa::make_version(0, 0, 1);
+void error_callback(int error, const char *description) {
+  TLOGF("glfw failed with %d, \n%s", error, description);
+}
+
 result_t initialize_early_infrastructure() {
   aeo::logging::initialize();
-  TLOGL("%s", "early initialization\n");
+  TLOGL("aeolipile (vulkan renderer library) version %d.%d.%d\n",
+        medusa::get_major(version), medusa::get_minor(version),
+        medusa::get_patch(version));
+
+  glfwSetErrorCallback(error_callback);
+  if (!glfwInit()) {
+    TLOGF("%s\n", "failed to initialize glfw!");
+    return -E_FAIL;
+  }
+
   return SUCCESS;
 }
+
+result_t deinitialize_early_infrastructure() {
+  glfwTerminate();
+  return SUCCESS;
+}
+
+GLFWwindow *init_window(const std::string title, const u32 width,
+                        const u32 height) {
+  GLFWwindow *window =
+      glfwCreateWindow(width, height, title.c_str(), NULL, NULL);
+
+  if (nullptr == window) {
+    TLOGF("failed to create a window (%s)\n", title.c_str());
+  }
+
+  return window;
+}
+
+u32 get_version() { return version; }
+
 }; // namespace aeo
