fe5e993cb129690cf935947aad8c1ea8b7a9f393
June-UwU <williamreji66@gmail.com>
2026-06-13 03:15:55 +0530

    [CORE] added basic window support 	1. added one error code. 	2. integrated glfw. 	3. added error string support. 	4. added example.
    Change-Id: I32d90cd2001af5441219148891226c3a0398cc55
    Signed-off-by: June-UwU <williamreji66@gmail.com>

--- example/example.cxx
+++ example/example.cxx
@@ -1,10 +1,23 @@
 #include "aeolipile.hpp"
 #include "types.hpp"
+#include <iostream>
 
 int main() {
   aeo::result_t result = aeo::initialize_early_infrastructure();
   if (true == aeo::is_error(result)) {
+    std::cout << aeo::result_to_string(result) << "\n";
     return -1;
   }
+
+  GLFWwindow *main_window = aeo::init_window("example", 1080, 720);
+  if (nullptr == main_window) {
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(main_window)) {
+  }
+
+  glfwDestroyWindow(main_window);
+  aeo::deinitialize_early_infrastructure();
   return 0;
 }
